9d598122cb36835827c9ccc5a3fd1c0d77414771
zachmolony <31472522+zachmolony@users.noreply.github.com>
2019-03-16 15:42:18 +0000

    added get user location and error handling

--- main.js
+++ main.js
@@ -1,8 +1,56 @@
+DALLAS_CHICKEN = {
+    lat: 51.4158,
+    lng: -0.1921
+}
+
+var map, infoWindow;
+
 function initMap() {
     var options = {
-        zoom:10,
-        center: {lat:51.4158, lng:-0.1921}
+        zoom: 10,
+        center: DALLAS_CHICKEN
     }
+    var map = new google.maps.Map(document.getElementById('map'), options);
 
-    var map = new google.maps.Map(document.getElementById('map'), options)
+    var dallas = new google.maps.Marker({
+        position: DALLAS_CHICKEN,
+        map: map,
+        //icon: 'assets/dallasicon.png'
+    });
+
+    // =============  USER LOCATION  =============
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var userPos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            var marker = new google.maps.Marker({
+                position: userPos,
+                map: map,
+            });
+
+            // ZOOM INTO ROUTE
+            var bounds = new google.maps.LatLngBounds();
+            bounds.extend(DALLAS_CHICKEN);
+            bounds.extend(userPos);
+            map.fitBounds(bounds, 200);
+
+        }, function () {
+            locationError(true, map.getCenter());
+        });
+    } else {
+        // NO GEOLOCATION
+        locationError(false, map.getCenter());
+    }
+};
+
+function locationError(browserHasGeolocation) {
+    if (browserHasGeolocation) {
+        $('#allowLocation').modal('errorMessage');
+    } else {
+        $('#allowLocation').innerHTML = "Please enable geolocation in your browser.";
+    }
 }
